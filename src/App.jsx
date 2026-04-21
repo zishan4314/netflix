dff03f47dab85750c2e2afc0fe0c1cacd1781a07
zishan4314 <shaikhzishan139@gmail.com>
2026-04-21 17:25:22 +0530

    fix: changed image paths to relative for production

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Login from "./login";
+import Login from "./components/Login"; // Adjust path if needed
 import  NetflixSeries  from "./components/NetflixSeries"; // Adjust path if needed
 
 function App() {

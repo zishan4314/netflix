36863ddb134f512019cf0bc43b343997d0927748
zishan4314 <shaikhzishan139@gmail.com>
2026-04-23 17:26:12 +0530

    Added node

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -3,22 +3,19 @@
 
 export const NetflixSeries = () => {
   return (
+    
     <div className="series-container">
-      <h1 className="main-heading" style={{ textAlign: "center", }}> Best Trending Series For You</h1>
+      <h1 className="main-heading"> Best Trending Series For You</h1>
       <ul className="grid-layout">
 {SeriesData.map((cureElem) => (
   <SeriesCard key={cureElem.id} data={cureElem}/>
-
 
 ))}
 
   </ul>
   </div>
-
 );
 
 
-
 };
-
 export default NetflixSeries;
